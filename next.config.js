2b577c71c79e58dd1fd20904aea5dd94ad0c60b0
Charaf Rezrazi <2086576+Rezrazi@users.noreply.github.com>
2022-12-03 20:10:18 +0100

    feat: added sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const { withSentryConfig } = require("@sentry/nextjs")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  sentry: {
+    hideSourceMaps: true,
+  },
 }
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  silent: true,
+}
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
