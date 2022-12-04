ebb75943137d4472459886c9ba60eef12ac01a9e
Charaf Rezrazi <2086576+Rezrazi@users.noreply.github.com>
2022-12-04 18:01:50 +0100

    feat: added fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,33 @@
-import "../styles/globals.css"
+import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { NextSeo } from "next-seo"
 import { ApolloProvider } from "@apollo/client"
 import client from "../utils/apollo"
 import Head from "next/head"
 import { Analytics } from "@vercel/analytics/react"
+import * as Fathom from "fathom-client"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load("QHUIQPQZ", {
+      includedDomains: ["stargazers.app", "www.stargazers.app"],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    router.events.on("routeChangeComplete", onRouteChangeComplete)
+
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
