08ac321ef6ceff2d86b90133d6433cb35c6a4431
Charaf Rezrazi <2086576+Rezrazi@users.noreply.github.com>
2022-12-04 14:04:45 +0100

    feat: ui improvements

--- pages/star/[session]/index.tsx
+++ pages/star/[session]/index.tsx
@@ -15,7 +15,7 @@ import PACKAGES_QUERY from "@/graphql/packages"
 import { GetServerSideProps } from "next"
 import client from "@/utils/apollo"
 import { captureException } from "@sentry/core"
-import Image from "next/image"
+import ProjectLogo from "@/components/ProjectLogo"
 
 interface Props {
   packages: Package[]
@@ -99,7 +99,7 @@ const Session = ({ session, packages: ssrPackages }: Props) => {
             </span>
           </h2>
           <div
-            className="w-full inline-flex justify-center items-center bg-zinc-600/10 gap-2 text-zinc-600 dark:text-zinc-300 rounded-lg py-4 px-2 hover:text-emerald-500 cursor-pointer"
+            className="w-full inline-flex justify-center items-center bg-zinc-200/50 dark:bg-zinc-600/10 gap-2 text-zinc-400 dark:text-zinc-400 dark:text-zinc-300 rounded-lg py-4 px-2 hover:text-emerald-500 cursor-pointer"
             onClick={handleCopyClick}
           >
             <code>{share}</code>
@@ -129,23 +129,8 @@ const Session = ({ session, packages: ssrPackages }: Props) => {
                     key={item.id}
                     className="cursor-pointer w-full text-left relative flex items-center py-4 px-4 py-4 hover:bg-emerald-500/5 gap-x-2"
                   >
-                    {item.image ? (
-                      <Image
-                        width={460}
-                        height={460}
-                        src={item.image}
-                        alt={item.name}
-                        className="w-10 h-10 rounded-lg"
-                      />
-                    ) : (
-                      <svg fill="currentColor" viewBox="0 0 24 24" className="w-10 h-10 text-black dark:text-white">
-                        <path
-                          fillRule="evenodd"
-                          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    )}
+                    <ProjectLogo src={item.image} alt={item.name} />
+
                     <div className="min-w-0 flex flex-col text-sm w-full gap-1">
                       <label
                         htmlFor={item.id}
